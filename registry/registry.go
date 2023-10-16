3f72794f87071a16d13d7836ab2fcd0614587822
goodliu <zenghao235711@gmail.com>
2023-10-16 20:48:05 +0800

    all: update copyright headers
    to keep consistent with the description in the CONTRIBUTING.md file of the trpc repository
    
    https://github.com/trpc-group/trpc/blob/main/CONTRIBUTING.md#copyright-headers

--- registry/registry.go
+++ registry/registry.go
@@ -1,8 +1,15 @@
+//
+//
 // Tencent is pleased to support the open source community by making tRPC available.
-// Copyright (C) 2023 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Copyright (C) 2023 THL A29 Limited, a Tencent company.
+// All rights reserved.
+//
 // If you have downloaded a copy of the tRPC source code from Tencent,
 // please note that tRPC source code is licensed under the Apache 2.0 License,
 // A copy of the Apache 2.0 License is included in this file.
+//
+//
 
 // Package registry 提供consul注册服务
 package registry
@@ -70,7 +77,7 @@ func (r *Registry) Register(service string, opts ...registry.Option) error {
 	if serviceOptions.TLSSkipVerify != nil {
 		TLSSkipVerify = *serviceOptions.TLSSkipVerify
 	}
-	//todo: http and grpc methods for registration.
+	// todo: http and grpc methods for registration.
 	check := &api.AgentServiceCheck{
 		Interval:                       serviceOptions.Interval,
 		Timeout:                        serviceOptions.Timeout,
